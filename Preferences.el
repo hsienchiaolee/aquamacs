205fe2f8572a91159e2655008445cca5d4ab9aa5
Kai Lee <klee@spotsetter.com>
2016-01-04 02:55:49 -0800

    Add name last macro and apply named macro to region shortcuts

--- Preferences.el
+++ Preferences.el
@@ -98,9 +98,25 @@
 (define-key osx-key-mode-map [(alt q)] 'erase-buffer)
 
 ;; macro
+(defun apply-named-macro-to-region-lines (top bottom)
+  "Apply named keyboard macro to all lines in the region."
+  (interactive "r")
+  (let ((macro (intern
+                (completing-read "kbd macro (name): "
+                                 obarray
+                                 (lambda (elt)
+                                   (and (fboundp elt)
+                                        (or (stringp (symbol-function elt))
+                                            (vectorp (symbol-function elt))
+                                            (get elt 'kmacro))))
+                                 t))))
+    (apply-macro-to-region-lines top bottom macro)))
+
 (define-key osx-key-mode-map [(alt \()] 'kmacro-start-macro)
 (define-key osx-key-mode-map [(alt \))] 'kmacro-end-macro)
 (define-key osx-key-mode-map [(alt \\) ?a] 'apply-macro-to-region-lines)
+(define-key osx-key-mode-map [(alt \\) ?s] 'name-last-kbd-macro)
+(define-key osx-key-mode-map [(alt \\) ?n] 'apply-named-macro-to-region-lines)
 
 ;; compiling
 (global-set-key [(meta ?`)] 'compile)
