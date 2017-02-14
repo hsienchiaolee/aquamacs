a7690701ce4adca99763db42bc5bb2a58bfc491b
Kai Lee <hlee027@gmail.com>
2017-02-14 02:35:20 -0800

    Update Preferences.el
    - New packages
      - use-package
      - helm
      - ensime
      - buffer-move

--- Preferences.el
+++ Preferences.el
@@ -1,16 +1,19 @@
+;; vertical window split
+(setq split-width-threshold nil)
+
 ;; indentation
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq-default py-indent-offset 2)
 (setq-default css-indent-offset 2)
 (setq-default c-basic-offset 2)
-(setq-default js2-basic-offset 2) ; same as tab-width
+(setq-default js2-basic-offset 2)
 (setq-default javascript-indent-level 2)
 (setq-default js-indent-level 2)
 (setq-default scala-indent-level 2)
 (custom-set-variables
-  '(python-guess-indent nil)
-  '(python-indent 2))
+ '(python-guess-indent nil)
+ '(python-indent 2))
 
 ;; auto complete
 (setq-default completion-ignore-case t)
@@ -21,24 +24,25 @@
 (put 'erase-buffer 'disabled nil)
 
 ;; packages
-(defun install-package (x)
-  (when (not (package-installed-p x)) (package-install x)))
-
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
+
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(install-package 'env-var-import)
-(install-package 'magit)
-(install-package 'buffer-move)
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(install-package 'flycheck)
+(eval-when-compile
+  (require 'use-package))
 
 ;; shell
+(add-to-list 'display-buffer-alist
+             '("^\\*shell\\*$" . (display-buffer-same-window)))
+
 (defun shell-named (name)
   (shell)
   (rename-buffer name)
@@ -65,7 +69,8 @@
 
 ;; files
 (defmacro hot-file (name)
-  `(lambda () (interactive) (find-file ,name)))
+  `(lambda () (interactive)
+     (find-file ,name)))
 
 (define-key osx-key-mode-map [(alt /) ?s] (hot-buffer "*scratch*"))
 (define-key osx-key-mode-map [(alt /) ?e] (hot-file "~/Library/Preferences/Aquamacs Emacs/Preferences.el"))
@@ -81,15 +86,32 @@
 (define-key osx-key-mode-map [(alt meta j)] 'insert-register)
 (define-key osx-key-mode-map [(alt meta k)] 'copy-to-register)
 
+;; helm
+(use-package helm
+  :ensure t
+  :init
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x C-m") 'helm-M-x)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  :config
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-z") 'helm-select-action)
+  )
+
 ;; magit
-(setq magit-auto-revert-mode nil)
 (define-key osx-key-mode-map [(alt m)] nil)
-(define-key osx-key-mode-map [(alt m) ?s] 'magit-status)
-(define-key osx-key-mode-map [(alt m) ?d] 'magit-diff)
-(define-key osx-key-mode-map [(alt m) ?c] 'magit-commit)
-(define-key osx-key-mode-map [(alt m) ?C] 'magit-commit-amend)
-(define-key osx-key-mode-map [(alt m) ?r] 'magit-rebase-interactive)
-(define-key osx-key-mode-map [(alt m) ?l] 'magit-log-current)
+(use-package magit
+  :ensure t
+  :init
+  (setq magit-auto-revert-mode nil)
+  :bind (("A-m s" . magit-status)
+         ("A-m d" . magit-diff)
+         ("A-m c" . magit-commit)
+         ("A-m C" . magit-commit-amend)
+         ("A-m r" . magit-rebase-interactive)
+         ("A-m m" . magit-merge)
+         ("A-m l" . magit-log-current)))
 
 ;; Lines
 (defun move-line (n)
@@ -144,12 +166,18 @@
 (define-key osx-key-mode-map [(alt \\) ?t] 'toggle-truncate-lines)
 
 ;; buffer
+(use-package buffer-move
+  :ensure t
+  :init
+  (define-key osx-key-mode-map [(meta alt left)] 'buf-move-left)
+  (define-key osx-key-mode-map [(meta alt right)] 'buf-move-right)
+  )
 (define-key osx-key-mode-map [(alt t)] 'new-empty-buffer)
 (define-key osx-key-mode-map [(alt r)] 'revert-buffer)
 (define-key osx-key-mode-map [(alt q)] 'erase-buffer)
 
-(define-key osx-key-mode-map [(meta alt left)] 'buf-move-left)
-(define-key osx-key-mode-map [(meta alt right)] 'buf-move-right)
+(define-key osx-key-mode-map [(ctrl right)] 'next-buffer)
+(define-key osx-key-mode-map [(ctrl left)] 'previous-buffer)
 
 ;; macro
 (defun apply-named-macro-to-region-lines (top bottom)
@@ -208,25 +236,65 @@
 
 ;; tramp
 (set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
+(define-key osx-key-mode-map [(alt \\) ?T] 'tramp-cleanup-all-connections)
+
+;; ensime
+(use-package ensime
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'exec-path "/opt/local/bin")
+  :config
+  (setq ensime-startup-snapshot-notification nil)
+  (setq ensime-startup-notification nil)
+  (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+  )
 
 ;; Go
-(env-var-import '("GOPATH"))
-(install-package 'go-mode)
-(install-package 'go-eldoc)
-(install-package 'go-autocomplete)
+(use-package env-var-import
+  :ensure t
+  :config
+  (env-var-import '("GOPATH"))
+  )
 
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'"
+  :interpreter "go"
+  :config
+  (defun go-mode-setup ()
+    (setq compile-command "go build -v && go test -v && go vet")
+    (define-key (current-local-map) "\C-c\C-c" 'compile)
+    (go-eldoc-setup)
+    (setq gofmt-command "goimports")
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (local-set-key [(alt b)] 'godef-jump))
+  (add-hook 'go-mode-hook 'go-mode-setup)
+  )
 
-(defun go-mode-setup ()
- (setq compile-command "go build -v && go test -v && go vet")
- (define-key (current-local-map) "\C-c\C-c" 'compile)
- (go-eldoc-setup)
- (setq gofmt-command "goimports")
- (add-hook 'before-save-hook 'gofmt-before-save)
- (local-set-key [(alt b)] 'godef-jump))
-(add-hook 'go-mode-hook 'go-mode-setup)
+(use-package go-eldoc
+  :ensure t
+  :defer t)
+
+(use-package go-autocomplete
+  :ensure t
+  :defer t
+  :config
+  (ac-config-default)
+  )
+
+;; swift
+(use-package swift-mode
+  :ensure t
+  :mode "\\.swift\\'"
+  :interpreter "swift")
+
+(use-package flycheck-swift
+  :ensure t
+  :defer t
+  :config
+  (flycheck-swift-setup)
+  )
 
 ;; start server
 (server-start)
