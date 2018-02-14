0e9e347e863fcb73a20d507971cbf22ed4a45e30
Kai Lee <hlee027@gmail.com>
2018-02-14 19:13:01 +0300

    Update emacs customization to have proper theme

--- customizations.el
+++ customizations.el
@@ -3,35 +3,53 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 307 t)
  '(aquamacs-tool-bar-user-customization nil t)
- '(default-frame-alist
-    (quote
-     ((cursor-type . box)
-      (vertical-scroll-bars . right)
-      (internal-border-width . 0)
-      (modeline . t)
-      (fringe)
-      (mouse-color . "black")
-      (cursor-color . "Red")
-      (background-mode . dark)
-      (tool-bar-lines . 0)
-      (menu-bar-lines . 1)
-      (right-fringe . 11)
-      (left-fringe . 3)
-      (background-color . "#000000")
-      (foreground-color . "#ffffff")
-      (font . "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-      (fontsize . 0)
-      (font-backend ns))))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-safe-themes
+   (quote
+    ("599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default)))
+
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(python-guess-indent nil)
  '(python-indent 2)
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
+ '(sort-fold-case t t)
  '(tabbar-mode nil nil (tabbar))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
  '(visual-line-mode nil t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
