a36152f165fb636457a410aef5f0cff70fedc6c0
Kai Lee <klee@tala.co>
2018-03-06 09:38:36 -0800

    Add ansible-vault package

--- Packages/settings/misc.el
+++ Packages/settings/misc.el
@@ -14,4 +14,12 @@
 (set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
 (define-key osx-key-mode-map [(alt \\) ?T] 'tramp-cleanup-all-connections)
 
+;; ansible valut
+(use-package ansible-vault
+  :ensure t
+  :pin melpa-stable
+  :defer t
+  :init
+  (setq ansible-vault-pass-file "~/.ansible-vault"))
+
 (provide 'misc)
