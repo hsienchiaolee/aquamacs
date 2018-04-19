41f48f887734dfe66610492dff8082f91e50c94b
Kai Lee <klee@tala.co>
2018-04-19 16:28:09 -0700

    Add SSH config file shortcut

--- Packages/settings/shortcut-file.el
+++ Packages/settings/shortcut-file.el
@@ -4,6 +4,7 @@
   ("s" (switch-to-buffer "*scratch*") "scratch" :exit t)
   ("e" (find-file "~/Library/Preferences/Aquamacs Emacs/Preferences.el") ".emacs" :exit t)
   ("b" (find-file "~/.bashrc") "bashrc" :exit t)
+  ("c" (find-file "~/.ssh/config") "SSH Config" :exit t)
   ("D" (find-file "/d") "/d" :exit t)
   ("q" nil "quit" :color blue))
 (global-set-key (kbd "A-/") 'hydra-file-shortcuts/body)
