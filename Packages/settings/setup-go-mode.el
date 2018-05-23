e36967a8dd727f0a9afe53b49d6e24f4937b28c3
Kai Lee <hlee027@gmail.com>
2018-05-23 12:04:36 -0700

    Add go-mode hydra

--- Packages/settings/setup-go-mode.el
+++ Packages/settings/setup-go-mode.el
@@ -17,7 +17,7 @@
   (defun go-mode-setup ()
     (setq tab-width 2)
     (setq compile-command "go build -v && go test -v && go vet")
-    (define-key (current-local-map) "\C-c\C-c" 'compile)
+    (define-key (current-local-map) (kbd "C-c C-c") 'compile)
     (go-eldoc-setup)
     (setq gofmt-command "goimports")
     (add-hook 'before-save-hook 'gofmt-before-save)
@@ -27,6 +27,24 @@
     (require 'go-flycheck)
     )
   (add-hook 'go-mode-hook 'go-mode-setup)
+
+  (defhydra hydra-go-mode (:color teal
+                           :hint nil)
+"
+     Playground                 Godef
+---------------------------------------------------
+ _p_: buffer                _d_: describe
+ _P_: region to buffer      _j_: jump
+ _A-p_: download entry      _J_: jump other window
+"
+    ("p"   go-play-buffer)
+    ("P"   go-play-region)
+    ("A-p" go-download-play)
+    ("d"   godef-describe)
+    ("j"   godef-jump)
+    ("J"   godef-jump-other-window)
+    ("q"   nil "cancel" :color blue))
+  (define-key go-mode-map (kbd "C-c h") `hydra-go-mode/body)
   )
 
 (use-package go-eldoc
