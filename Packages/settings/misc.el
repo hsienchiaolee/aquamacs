c2f35140f82bb3db3db859303086613181edc6b9
Kai Lee <hlee027@gmail.com>
2018-06-01 09:29:42 -0700

    Add dirtrack shell setup

--- Packages/settings/misc.el
+++ Packages/settings/misc.el
@@ -22,4 +22,13 @@
   :init
   (setq ansible-vault-pass-file "~/.ansible-vault"))
 
+;; dirtrack
+(add-hook 'shell-mode-hook
+  (lambda ()
+    (shell-dirtrack-mode 0)
+    ;(setq ssh-directory-tracking-mode 'ftp)
+    (setq dirtrack-list '("[A-Za-z] \\([~/\\].*\\)[$]" 1))
+    ;(dirtrack-debug-mode)
+    (dirtrack-mode)))
+
 (provide 'misc)
