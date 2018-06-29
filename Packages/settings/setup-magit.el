c0e62a098e505adeb8452d0523f75e1597232a55
Kai Lee <hlee027@gmail.com>
2018-06-29 08:55:54 -0700

    Cleanup magit hydra

--- Packages/settings/setup-magit.el
+++ Packages/settings/setup-magit.el
@@ -5,18 +5,33 @@
   :init
   (setq magit-auto-revert-mode nil)
   :config
-  (defhydra hydra-magit (:color blue
-                         :columns 4)
-    "magit"
-    ("RET" magit-visit-thing "visit magit things at point")
-    ("s" magit-status "status")
-    ("d" magit-diff "diff")
-    ("c" magit-commit "commit")
-    ("C" magit-commit-amend "commit-amend")
-    ("r" magit-rebase-interactive "rebase-interactive")
-    ("m" magit-merge "merge")
-    ("l" magit-log-current "log")
-    ("q" nil "exit")
+  (defhydra hydra-magit (:color teal :hint nil)
+"
+                   Magit
+
+  Status        Changes           Rebase            Cherry Pick       Merge
+--------------------------------------------------------------------------------------
+_s_: status      _c_: commit        _r_: rebase          _p_: cherry pick    _m_: merge
+_d_: diff        _C_: amend         _A-r_: continue      _A-p_: continue
+_l_: log         _h_: reset        
+_f_: reflog      _H_: reset hard
+
+"
+    ("s" magit-status)
+    ("d" magit-diff)
+    ("l" magit-log-current)
+    ("f" magit-reflog)
+    ("c" magit-commit)
+    ("C" magit-commit-amend)
+    ("h" magit-reset-head)
+    ("H" magit-reset-hard)
+    ("r" magit-rebase-interactive)
+    ("A-r" magit-rebase-continue)
+    ("p" magit-cherry-pick)
+    ("A-p" magit-sequencer-continue)
+    ("m" magit-merge)
+    ("q" nil "cancel" :color blue)
+    ("RET" magit-visit-thing)
     )
   (global-set-key (kbd "A-m") 'hydra-magit/body)
   )
