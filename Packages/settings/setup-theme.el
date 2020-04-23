dc4393f83074b563d953874be18939cda4df107d
Kai Lee <hlee027@gmail.com>
2020-04-23 17:43:18 -0400

    Update theme from Zenburn to spacemacs theme

--- Packages/settings/setup-theme.el
+++ Packages/settings/setup-theme.el
@@ -1,7 +1,25 @@
-(use-package zenburn-theme
-  :ensure t
-  :config
+(use-package spacemacs-theme
+  :defer t
+  :init
   (setq default-frame-alist nil)
-  (load-theme 'zenburn t))
+  (load-theme 'spacemacs-dark t)
+  )
+
+(use-package spaceline
+  :defer t
+  :init
+  (setq powerline-default-separator 'arrow-fade)
+  )
+
+(use-package spaceline-config
+  :demand t
+  :ensure spaceline
+  :config
+  (spaceline-define-segment line-column
+    "The current line and column numbers."
+    "l:%l c:%2c")
+  (spaceline-helm-mode 1)
+  (spaceline-spacemacs-theme)
+  )
 
 (provide 'setup-theme)
