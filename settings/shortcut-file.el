141d8f06355f5b62f131b736e6de8c6a7b691a7e
Kai Lee <klee@stripe.com>
2022-11-01 11:50:49 -0700

    Set bashrc shortcut to ~/.bash/interactive

--- settings/shortcut-file.el
+++ settings/shortcut-file.el
@@ -3,7 +3,7 @@
   "files"
   ("s" (switch-to-buffer "*scratch*") "scratch" :exit t)
   ("e" (find-file "~/Library/Preferences/Aquamacs Emacs/Preferences.el") ".emacs" :exit t)
-  ("b" (find-file "~/.bashrc") "bashrc" :exit t)
+  ("b" (find-file "~/.bash/interactive") "bashrc" :exit t)
   ("n" (find-file "~/workspace/org/notes.org") "notes" :exit t)
   ("c" (find-file "~/.ssh/config") "SSH Config" :exit t)
   ("D" (find-file "/d") "/d" :exit t)
