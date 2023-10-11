765ad0f8147f7b7b8439a9a4b54bb4c2581653ad
Kai Lee <hlee027@gmail.com>
2023-10-10 18:07:59 -0700

    Update shortcut-file

--- .emacs.d/settings/shortcut-file.el
+++ .emacs.d/settings/shortcut-file.el
@@ -4,10 +4,9 @@
   ("s" (switch-to-buffer "*scratch*") "scratch" :exit t)
   ("e" (find-file "~/Library/Preferences/Aquamacs Emacs/Preferences.el") ".emacs" :exit t)
   ("b" (find-file "~/.bash/interactive") "bashrc" :exit t)
-  ("n" (find-file "~/workspace/org/notes.org") "notes" :exit t)
+  ("o" (find-file "~/Documents/org") "notes" :exit t)
   ("c" (find-file "~/.ssh/config") "SSH Config" :exit t)
-  ("D" (find-file "/d") "/d" :exit t)
-  ("D" (find-file "~/d") "/d" :exit t)
+  ("d" (find-file "~/d") "tmp" :exit t)
   ("q" nil "quit" :color blue))
 (global-set-key (kbd "s-/") 'hydra-file-shortcuts/body)
 
